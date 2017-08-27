49731bc99941765200d69ddf0766ccd2ffa8a700
hkums <hkums@allstate.com>
2017-08-27 14:06:47 +0530

    css style inline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles : [`
+    h1{
+    color: darkblue;
+  `],
 })
 export class AppComponent {
   title = 'app';
