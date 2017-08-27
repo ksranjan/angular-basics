bab1789e9742bd0d1219fa4e394c25b38f3b57bb
hkums <hkums@allstate.com>
2017-08-27 15:46:39 +0530

    Event binding moving towards 2 way binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent implements OnInit {
 
   allowNewServer = false;
   serverCreationStatus = 'No servers created';
+  serverName = '';
 
   constructor() {
     setTimeout( () => {
@@ -21,5 +22,8 @@ export class ServersComponent implements OnInit {
 
   onCreateServer(){
     this.serverCreationStatus = 'New server created';
+  }
+  onUpdateServerName(event:Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
